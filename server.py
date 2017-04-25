2755fdb45d671192f78dac8a115c4c0d514981a4
qiuyang57 <qiuyang57@gmail.com>
2017-04-25 20:22:07 +1000

    three times login block

--- server.py
+++ server.py
@@ -3,30 +3,35 @@ from socket import *
 import threading
 
 TIMEOUT = 100
-BLOCK_DURATION = 10
+BLOCK_DURATION = 15
 
 
 class ConnectionThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
 
+
     def run(self):
         while True:
             connectionSocket, addr = serverSocket.accept()
-            newthread = ClientThread(connectionSocket)
-            newthread.start()
-            threads.append(newthread)
-            sockets.append(connectionSocket)
+            if addr[0] not in server_blockdict:
+                newthread = ClientThread(connectionSocket, addr)
+                newthread.start()
+                threads.append(newthread)
+                sockets.append(connectionSocket)
+            else:
+                connectionSocket.send("block".encode())
 
 
 class ClientThread(threading.Thread):
-    def __init__(self, sock):
+    def __init__(self, sock, addr):
         threading.Thread.__init__(self)
         self.sock = sock
         # self.init_time = init_time
         self.login_require_flag = True
         self.login_remain_times = 2
         self.message = ""
+        self.addr = addr
 
     def timeout_reset(self):
         timeout_dict[self.sock] = time()
@@ -38,13 +43,13 @@ class ClientThread(threading.Thread):
             word = self.message.split()
             print(word)
             reply = "wrong"
+            if self.login_remain_times == 0:
+                reply = "block"
+                server_blockdict[self.addr[0]]=time()
+                print(server_blockdict)
             if self.message == "":
                 print("connection lost")
                 break
-            if self.login_remain_times == 0:
-                reply = "block"
-                # server_blocklist.append([addr[0], time()])
-                print(server_blocklist)
             print(word[1], credentials.keys())
             if word[1] in credentials.keys():
                 if credentials[word[1]] == word[2]:
@@ -63,15 +68,25 @@ class ClientThread(threading.Thread):
 
 
 def check_timeout():
-    socktoclose = []
+    socktoClose = []
     for sock in timeout_dict:
         time_now = time()
         if time_now - timeout_dict[sock] > TIMEOUT:
             sock.send("timeout".encode())
-            socktoclose.append(sock)
-    for sock in socktoclose:
+            socktoClose.append(sock)
+    for sock in socktoClose:
         del timeout_dict[sock]
         sock.close()
+
+
+def update_serverblockdict():
+    iptoRemove = []
+    for ip in server_blockdict:
+        time_now = time()
+        if time_now - server_blockdict[ip] > BLOCK_DURATION:
+            iptoRemove.append(ip)
+    for ip in iptoRemove:
+        del server_blockdict[ip]
 
 
 
@@ -85,17 +100,24 @@ with open("credentials.txt") as file:
         credentials[word[0]] = word[1]
 serverSocket.listen(5)
 print("The server is ready to receive")
-server_blocklist = []
+
+server_blockdict = {}
 threads = []
 sockets = []
 timeout_dict = {}
+
+
 con_thread = ConnectionThread()
 con_thread.start()
+prev_time= time()
 while True:
     # print(sockets)
     check_timeout()
+    cur_time = time()
+    if cur_time-prev_time>1:
+        update_serverblockdict()
+        prev_time = cur_time
     # print(threads)
-    # print(addr)
 connectionSocket.close()
 for t in threads:
     t.join()
